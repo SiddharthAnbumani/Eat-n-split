6f8167c169506847b720b5fe05786fe331d979ea
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-02-11 06:01:45 +0530

    Moving The FriendsList into a separate JSX and Importing them in App.jsx

--- FriendsList.jsx
+++ FriendsList.jsx
@@ -1,1 +1,15 @@
+export default function FriendsList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          selectedFriend={selectedFriend}
+          onSelection={onSelection}
+        />
+      ))}
+    </ul>
+  );
+}
 
